eeefb0e6f864cf2aa4d718e071e7d092f94abca5
dylan <dylancjc08@1utar.my>
2026-04-06 15:59:44 +0800

    second commit

--- src/test/java/com/example/HelloWorldTest.java
+++ src/test/java/com/example/HelloWorldTest.java
@@ -1,11 +1,11 @@
 package com.example;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
 public class HelloWorldTest {
     @Test
     public void testGetGreeting() {
         HelloWorld helloWorld = new HelloWorld();
-        assertEquals("Hello, World!", helloWorld.getGreeting());
+        assertEquals("Hello, Student!", helloWorld.getGreeting());
     }
 }
